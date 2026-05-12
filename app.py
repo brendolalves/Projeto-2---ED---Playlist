1d00cc6d7bbd22035c392605052cb20b834e45f8
brendolalves <brendol.alves@gmail.com>
2026-05-12 11:35:11 -0300

    função main
    criação das variaveis e da opção 1

--- app.py
+++ app.py
@@ -18,3 +18,26 @@ def exibir_menu():
     print("10. Sair")
     return input("Escolha uma opção: ")
 
+def main():
+    bib = Biblioteca()
+    f_relaxar = Fila()
+    f_focar = Fila()
+    f_animar = Fila()
+    f_treinar = Fila()
+    historico = Fila()
+
+    while True:
+        opçao = exibir_menu
+
+        if opçao == "1": 
+            try:
+                titulo = input("Titulo: ")
+                artista = input("Artista: ")
+                genero = input("Gênero: ")
+                bpm = int(input("BPM: "))
+                if bpm <= 0: raise ValueError
+                bib.adicionar(titulo, artista, genero, bpm)
+            except ValueError:
+                print("Erro: BPM deve ser um número inteiro positivo.")
+
+
